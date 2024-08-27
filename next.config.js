957065d1e8e9676fa6e55d18ce7559af97da3f2e
sachweb <ssmewada1423@gmail.com>
2024-08-27 12:34:37 +0530

    Deploy second

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/testapp', // e.g. '/kavach-app'
+    basePath: '/testapp', 
     assetPrefix: '/testapp/',
     typescript: {
         ignoreBuildErrors: true,
     }
 };
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  })
+  
 
-module.exports = nextConfig;
+module.exports =  withBundleAnalyzer(nextConfig);
