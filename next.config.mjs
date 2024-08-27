b7dbada525b92072e97534d0042d0e779083844e
sachweb <ssmewada1423@gmail.com>
2024-08-27 11:04:04 +0530

    make build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/testapp', // e.g. '/kavach-app'
+    assetPrefix: '/testapp/',
+};
 
 export default nextConfig;
